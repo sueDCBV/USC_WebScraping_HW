c85876353373d1d3cd5b39d3b6aa93a7d103e292
SUE HELLEN DEL CARPIO BELLIDO VARGAS <sueDCBV@gmail.com>
2018-05-11 18:04:40 -0700

    tweet

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "eALz2cQSEgfaD9wGF6IUo71fi"
+consumer_secret = "plaPgmEfihCY35ItgTZ3FyeZmFUgDJjhjOXRWNNKBdP3bPn5nh"
+access_token = "1442700439-8mRuFCe7zIStOzilhSIfO2Eep7rYM83hRWpEW9o"
+access_token_secret = "lu8v2F8TBaD1t3Sp3Qn0x3jhpdxdnGiWCRmmzSPxECLmO"
